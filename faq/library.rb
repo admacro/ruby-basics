f783d8968c2b92cae62ca327ba486d6202c33778
James Ni <admacro@gmail.com>
2018-01-26 19:15:23 +0800

    file reading and writing

--- faq/library.rb
+++ faq/library.rb
@@ -16,4 +16,39 @@ srand 23 # set a constant seed (before calling rand)
 end
 
 
-# 
+# file read
+File.open("random.txt", "r+").readlines.each_with_index do |line, i|
+  # readlines returns the lines in an array
+  line[0, 0] = "#{i + 1}: " # string[start, length]
+  p line.class # => String
+  p line # => "3: 0.7654597593969069\r\n" (when i = 2)
+end
+
+# file write
+File.open("random.txt", "r+") do |f|
+  lines = f.readlines
+  lines.each_with_index { |line, i| line[0, 0] = "#{i + 1}: " }
+  f.rewind # positions f to the beginning of input, resetting lineno to zero
+  f.puts lines # writes lines to file io stream
+end
+
+p File::SEPARATOR # "/" on Windows
+
+
+# one line script for editing file in-place (add line numbers to a file)
+# ruby -i.bak -ne 'print "#$.: #$_"' random.txt
+
+# ruby [switches] [--] [programfile] [arguments]
+#  -i[extension] => edit ARGV files in place (make backup if extension supplied) (random.txt.bak)
+#    ARGV => command line arguments (in an array) when script is run
+#  -n => assume 'while gets(); ... end' loop around your script
+#  -e 'command' => one line of script. Several -e's allowed. Omit [programfile]
+#  $. => The current input line number of the last file that was read
+#  $_ => The last input line of string by gets or readline
+#  gets => Returns (and assigns to $_) the next line from the list of files in
+#    ARGV (or $*), or from standard input if no files are present on the command line.
+#    Returns nil at end of file.
+
+p ARGV # => ["test"] if run with 'ruby library.rb test'
+p ARGV.class # => Array
+
