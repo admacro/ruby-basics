ba31ad8564f29b73975cc78c2183effbf9ce3feb
James Ni <admacro@gmail.com>
2018-03-17 23:45:26 +0800

    correct comment

--- rails_guides/blog/app/models/account.rb
+++ rails_guides/blog/app/models/account.rb
@@ -4,6 +4,6 @@ class Account < ApplicationRecord
   # %w(foo bar #{1+1}) == ["foo", "bar", "\#{1+1}"]
   # %W(foo bar #{1+1}) == ["foo", "bar", "2"] # capitalized
 
-  # similar with exclusion, inclusion validates that the field value is in a given set
+  # contrary to exclusion, inclusion validates that the field value is in a given set
   validates :subdomain, inclusion: { in: %w(blog admin portfolio), message: "%{value} is not a subdomain"}
 end
