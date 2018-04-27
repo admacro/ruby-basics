8bcb653932ada60139017cd4bf91bbbea1f9f862
James <james.ni@strikingly.com>
2018-04-27 11:30:14 +0800

    add comments to the simple Rack app

--- rack_server/simple_rack_app.rb
+++ rack_server/simple_rack_app.rb
@@ -3,6 +3,16 @@
 
 require "rack"
 
+# To use Rack, provide an "app": an object that responds to the call method,
+# taking the environment hash as a parameter, and returning an Array with
+# three elements:
+# 
+#   - The HTTP response code
+#   - A Hash of headers
+#   - The response body, which must respond to each
+
+# The reason you can pass a Proc object to run is Proc responds to call.
+# Check Ruby doc for details.
 app = Proc.new do |env|
   ['200', {'Content-Type' => 'text/html'}, ['Hello Rack!']]
 end
