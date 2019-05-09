dd93b08549bb143583019c88421e8353818924ea
James Ni <admacro@gmail.com>
2019-05-10 00:33:09 +0800

    add example for Kernel.trap

--- faq/trap.rb
+++ faq/trap.rb
@@ -1,9 +1,18 @@
 # coding: utf-8
 # ruby
 
-# Trap ^C 
-Signal.trap("INT") { 
-  puts "\nShutting down gracefully..." 
+# Trap ^C
+Signal.trap("INT") {
+  puts "\n Goodby..."
+  exit
+}
+
+# Trap ^\
+# This is on mac. It might be different on other systems.
+# Use `stty -a` to list signals and their associated key combo
+# Kernel.trap (synonymous with Signal.trap)
+trap("QUIT") {
+  puts "\nShutting down gracefully..."
   exit
 }
 
